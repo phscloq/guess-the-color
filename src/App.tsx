68d5554dcf2967c4ac6af58a9e247cdd9fc2db9e
Baran Dogan <phscloq@gmail.com>
2024-07-29 23:57:40 +0300

    Add score display and tracking to the game

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,42 @@ import './App.css'
 
 function App() {
   const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-
   const [color, setColor] = useState<string>("");
   const [answers, setAnswers] = useState<string []>([]);
   const [statement, setStatement] = useState<string>("");
+  const [score, setScore] = useState<number>(0);
+
   const getColor = ()=>{
     let newColor = '#';
     for(let i=0; i<6; i++){
       newColor += digits[Math.floor(Math.random()* digits.length )];
-      console.log(newColor);
-      console.log(i);
     }
-    setColor(newColor); 
     return newColor;
   }
 
   useEffect(()=>{
-    getColor();
-    const newAnswers = [color, getColor(), getColor()].sort(() => Math.random() - 0.5);
+    const rightColor = getColor();
+    setColor(rightColor);
+    const newAnswers = [rightColor, getColor(), getColor()].sort(() => Math.random() - 0.5);
     setAnswers(newAnswers);
   },[])
 
   const handleClick = (e:any)=>{
     if(e.target.innerHTML === color){
       const rightColor = getColor();
+      const newScore = score;
       setColor(rightColor);
       setAnswers([rightColor, getColor(), getColor()].sort(() => Math.random() - 0.5));
       setStatement('Correct!');
+      setScore(newScore+1);
   }
   else{
+      setScore(0);
       setStatement('Wrong!');
   }
   }
+  
+
   return (
     <>
       <div className='App'>
@@ -49,7 +53,10 @@ function App() {
               })}
             </div>
             <p className={`${statement== 'Correct!' ? 'correct-text': 'wrong-text'}`}>{statement}</p>
-
+        </div>
+        <div className='score-container'>
+          <h2 className='score-title'>Score</h2>
+          <p className='score-number'>{score}</p>
         </div>
       </div>
 
